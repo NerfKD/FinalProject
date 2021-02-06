a06e136452d1359cb26e50a2c3e1b9da728b89bd
Kerim Dinçer <kerim_dincer@hotmail.com>
2021-02-06 18:59:36 +0300

    n

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,19 +1,33 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //Open Closed Principle - yaptığın yazılıma yeni bir özellik eklendiğinde mevcutta ki koduna dokunmamalısın.
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
             foreach (var product in productManager.GetAll())
             {
                 Console.WriteLine(product.ProductName);
-                
+            }
+
+            //Kategori Id si 1 olanları getir.
+            foreach (var product in productManager.GetAllByCategoryId(1))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            //UnitPrice en az 50 en fazla 100 olanları getir.
+            foreach (var product in productManager.GetByUnitPrice(50, 100))
+            {
+                Console.WriteLine(product.ProductName);
             }
         }
     }
