ff783d2f35fc7e360dd2cb11a73c51a2657e534e
Kerim Dinçer <kerim_dincer@hotmail.com>
2021-03-02 00:24:15 +0300

    H7G1

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,6 +10,10 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string ProductsListed = "Ürünler listelendi";
-
+        public static string ProductCountOfCategoryError = "Kategori ürün limiti aşılmıştır.";
+        public static string ProductUpdated = "Ürün güncellendi.";
+        public static string ProductNameAlreadyExists = "Aynı isme sahip ürün mevcut.";
+        public static string MaxCategoryCount = "Kategori sayısı limiti aşılmıştır.";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
     }
 }
--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
+using Business.CCS;
 using Business.Concrete;
 using Castle.DynamicProxy;
 using Core.Utilities.Interceptors;
@@ -20,7 +21,8 @@ namespace Business.DependencyResolvers.Autofac
         {
             builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
             builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
-
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
             //load olurken herhangi bir ıterceptor varmı bak varsa çalıştır.
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
