4c076ce1d44fec96d523f69b9b2e99b28e04f097
Kerim Dinçer <kerim_dincer@hotmail.com>
2021-03-24 21:25:48 +0300

    neww

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -50,11 +50,11 @@ namespace Business.Concrete
         [CacheAspect] //key,value
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //if (DateTime.Now.Hour == 22)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
 
-            }
+            //}
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
